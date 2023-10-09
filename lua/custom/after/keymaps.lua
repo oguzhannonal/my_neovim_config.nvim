fe64e3f56129029716074b46dfcb730aaa36683f
OguzhanOnal <oguzhanonal5252@gmail.com>
2023-10-09 22:32:33 +0300

    feat: new git diff function

--- lua/custom/after/keymaps.lua
+++ lua/custom/after/keymaps.lua
@@ -21,13 +21,60 @@ nmap('<A-1>', '<Cmd>:lua require("harpoon.ui").nav_file(1)<cr>', 'Harpoon Nav Fi
 nmap('<A-2>', '<Cmd>:lua require("harpoon.ui").nav_file(2)<cr>', 'Harpoon Nav File 2')
 nmap('<A-3>', '<Cmd>:lua require("harpoon.ui").nav_file(3)<cr>', 'Harpoon Nav File 3')
 nmap('<A-4>', '<Cmd>:lua require("harpoon.ui").nav_file(4)<cr>', 'Harpoon Nav File 4')
+-- function _G.git_diff(opts)
+--   local pickers = require 'telescope.pickers'
+--   local finders = require 'telescope.finders'
+--   local conf = require('telescope.config').values
+--   local current_branch = vim.fn.system 'git rev-parse --abbrev-ref HEAD'
+--   local cmd = string.format('git diff --name-only %s', current_branch)
+--   local list = vim.fn.systemlist(cmd)
+--   pickers
+--     .new(opts, {
+--       prompt_title = 'git diff',
+--       finder = finders.new_table { results = list },
+--       sorter = conf.generic_sorter(opts),
+--     })
+--     :find()
+-- end
 function _G.git_diff(opts)
   local pickers = require 'telescope.pickers'
   local finders = require 'telescope.finders'
   local conf = require('telescope.config').values
-  local current_branch = vim.fn.system 'git rev-parse --abbrev-ref HEAD'
-  local cmd = string.format('git diff --name-only %s', current_branch)
-  local list = vim.fn.systemlist(cmd)
+
+  -- Get the current branch
+  local current_branch = vim.fn.system('git rev-parse --abbrev-ref HEAD'):gsub('\n', '')
+
+  -- Specify the main branch
+  local main_branch = 'dev'
+
+  -- Use git diff to get the list of uncommitted changes in the current branch
+  local uncommitted_cmd = 'git diff --name-only'
+  local uncommitted_list = vim.fn.systemlist(uncommitted_cmd)
+
+  -- Use git diff to get the list of changed files between the current branch and main branch
+  local diff_cmd = string.format('git diff --name-only %s..%s', main_branch, current_branch)
+  local diff_list = vim.fn.systemlist(diff_cmd)
+
+  -- Create a Set to store unique file names
+  local unique_files = {}
+
+  -- Add files from uncommitted_list to the set
+  for _, file in ipairs(uncommitted_list) do
+    unique_files[file] = true
+  end
+
+  -- Add files from diff_list to the set
+  for _, file in ipairs(diff_list) do
+    unique_files[file] = true
+  end
+
+  -- Convert the Set back to a table
+  local list = {}
+  for file, _ in pairs(unique_files) do
+    table.insert(list, file)
+  end
+
+  -- Create a telescope picker to display the list of uncommitted changes and changes between branches
   pickers
     .new(opts, {
       prompt_title = 'git diff',
