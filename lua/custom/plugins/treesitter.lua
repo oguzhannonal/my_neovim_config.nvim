1cbdf6a6fe0a08bc3a98f2a41f2929cdb60ffba4
OguzhanOnal <oguzhanonal5252@gmail.com>
2023-10-20 23:33:42 +0300

    feat: session

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -17,7 +17,7 @@ return {
       },
       auto_install = false,
       autotag = { enable = true },
-      highlight = { enable = true },
+      highlight = { enable = true, additional_vim_regex_highlighting = false },
       indent = { enable = true },
       incremental_selection = {
         enable = true,
