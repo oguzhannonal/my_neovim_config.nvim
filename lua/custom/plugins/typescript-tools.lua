7a3f7ccd1c424d053330a575003e17aded2acaf1
Oguzhan <oguzhanonal5252@gmail.com>
2024-05-01 14:09:16 +0300

    feat: modif

--- lua/custom/plugins/typescript-tools.lua
+++ lua/custom/plugins/typescript-tools.lua
@@ -1,7 +1,7 @@
 return {
-  'pmizio/typescript-tools.nvim',
-  requires = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-  config = function()
-    require('typescript-tools').setup {}
-  end,
+  -- 'pmizio/typescript-tools.nvim',
+  -- requires = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  -- config = function()
+  --   require('typescript-tools').setup {}
+  -- end,
 }
